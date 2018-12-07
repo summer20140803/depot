f1bbda66e68ef02a249b6935015ec4720d0ae4ac
oufen <1263759508@qq.com>
2018-12-07 18:00:47 +0800

    progress: p143

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,7 +27,6 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-    # 下面写法等同于 @line_item = LineItem.new(cart: @cart, product: product)
     @line_item = @cart.add_product(product)
 
     session[:visit_counter] = 0
@@ -35,7 +34,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         # @line_item.cart 指向了 /carts/:id(.:format)，即触发了 carts#show 方法
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -76,6 +75,6 @@ class LineItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -6,8 +6,18 @@ class Cart < ApplicationRecord
     if current_item
       current_item.quantity += 1
     else
-      current_item = line_items.build(product_id: product.id)
+      # 下面的方法等同于 current_item = LineItem.new(product_id: product.id, cart_id: self.id)
+      current_item = self.line_items.build(product_id: product.id)
     end
     current_item
+  end
+
+  def total_price
+    # line_items返回的对象是 <LineItem::ActiveRecord_Associations_CollectionProxy:0x00007fb946ea8d58>
+    # 需要通过 #to_a 转换成Ruby数组
+    line_items.to_a.sum do |item|
+      logger.error "#{item}"
+      item.total_price
+    end
   end
 end
